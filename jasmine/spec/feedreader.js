adb71a5095cd6088301dc53e1cc422e51a5160c3
Vlad Sivirin <sivirin@gmail.com>
2018-05-16 19:17:15 +0300

    Added 'The menu' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,6 +82,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', () => {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -94,6 +95,8 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
