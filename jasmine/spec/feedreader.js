7d8c064d42c0487b96a768aa26248de3ffbf13d1
Vlad Sivirin <sivirin@gmail.com>
2018-05-18 19:47:32 +0300

    Added 'Initial Entries' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -106,6 +106,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -113,6 +114,8 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
