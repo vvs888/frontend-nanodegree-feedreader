331f6a337763ba29b3cbadbe84cb2fb390cbfd51
Vlad Sivirin <sivirin@gmail.com>
2018-05-19 19:29:59 +0300

    Added 'New Feed Selection' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -129,9 +129,12 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', () => {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
+
 }());
